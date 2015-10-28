2be6b5eae00855402ac1506b0c5656525ee6dadf
kpandurangarao <kpandurangarao@kpandurangarao.pkrmsez.com>
2015-10-28 12:37:49 +0530

    groovy

--- slf4j-example/test.groovy
+++ slf4j-example/test.groovy
@@ -2,7 +2,7 @@ freeStyleJob('example') {
     logRotator(-1, 10)
     jdk('Java 6')
     scm {
-        github('https://github.com/kpandurangarao/pandurepo.git/TestRestExample', 'master')
+        github('https://github.com/kpandurangarao/pandurepo.git', 'master')
     }
     triggers {
         githubPush()
