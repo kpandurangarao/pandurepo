0f4e80bc6331fd1cf0723b60da1a6547bc7e8b1f
kpandurangarao <kpandurangarao@kpandurangarao.pkrmsez.com>
2015-10-28 12:29:07 +0530

    groovy update

--- slf4j-example/test.groovy
+++ slf4j-example/test.groovy
@@ -2,15 +2,12 @@ freeStyleJob('example') {
     logRotator(-1, 10)
     jdk('Java 6')
     scm {
-        github('jenkinsci/job-dsl-plugin', 'master')
+        github('https://github.com/kpandurangarao/pandurepo.git/TestRestExample', 'master')
     }
     triggers {
         githubPush()
     }
     steps {
         gradle('clean build')
-    }
-    publishers {
-        archiveArtifacts('job-dsl-plugin/build/libs/job-dsl.hpi')
     }
 }
