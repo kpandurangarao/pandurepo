c2abd36d34a65666586ed8d51f699488c9556fcf
kpandurangarao <kpandurangarao@kpandurangarao.pkrmsez.com>
2015-10-28 12:19:18 +0530

    groovy file updated

--- slf4j-example/test.groovy
+++ slf4j-example/test.groovy
@@ -1,7 +1,16 @@
-job('Example dsl') {
+freeStyleJob('example') {
+    logRotator(-1, 10)
+    jdk('Java 6')
+    scm {
+        github('jenkinsci/job-dsl-plugin', 'master')
+    }
+    triggers {
+        githubPush()
+    }
     steps {
-        shell('''
-        	echo "hello world today"
-    	'''.stripIndent())
+        gradle('clean build')
+    }
+    publishers {
+        archiveArtifacts('job-dsl-plugin/build/libs/job-dsl.hpi')
     }
 }
